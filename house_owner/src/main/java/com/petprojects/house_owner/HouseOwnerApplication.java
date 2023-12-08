1b91e2a9b043b1ede68fa3932d243dbc1d52f368
reshetovProg <94145533+reshetovProg@users.noreply.github.com>
2023-12-08 21:26:14 +0300

    Add files via upload

---
+++ house_owner/src/main/java/com/petprojects/house_owner/HouseOwnerApplication.java
@@ -0,0 +1,14 @@
+package com.petprojects.house_owner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseOwnerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HouseOwnerApplication.class, args);
+	}
+
+}
---
+++ house_owner/src/test/java/com/petprojects/house_owner/HouseOwnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petprojects.house_owner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseOwnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
